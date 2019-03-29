a631b74e8ab3675dd39398e7612aa899b2da6341
jiujuanfeng <jiujuanfeng@163.com>
2019-03-30 01:23:02 +0800

    init project

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\SiteMap;
+use App\Services\RssFeed;
+use App\Models\Post;
+use App\Services\PostService;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index(Request $request)
+    {
+        $tag = $request->get('tag');
+        $postService = new PostService($tag);
+        $data = $postService->lists();
+        $layout = $tag ? Tag::layout($tag) : 'blog.layouts.index';
+        return view($layout, $data);
+    }
+
+    public function showPost($slug, Request $request)
+    {
+        $post = Post::with('tags')->where('slug', $slug)->firstOrFail();
+        $tag = $request->get('tag');
+        if ($tag) {
+            $tag = Tag::where('tag', $tag)->firstOrFail();
+        }
+
+        return view($post->layout, compact('post', 'tag'));
+    }
+
+    public function rss(RssFeed $feed)
+    {
+        $rss = $feed->getRSS();
+        return response($rss)->header('Content-type', 'application/rss+xml');
+    }
+
+    public function siteMap(SiteMap $siteMap)
+    {
+        $map = $siteMap->getSiteMap();
+
+        return response($map)->header('Content-type', 'text/xml');
+    }
+}
---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\ContactMeRequest;
+use App\Mail\ContactMail;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function showForm()
+    {
+        return view('blog.contact');
+    }
+
+    public function sendContactInfo(ContactMeRequest $request)
+    {
+        $data = $request->only('name', 'email', 'phone');
+        $data['messageLines'] = explode('\n', $request->get('message'));
+
+        Mail::to($data['email'])->queue(new ContactMail($data));
+
+        return back()->with('success', "消息已经发送，感谢你的反馈");
+    }
+}
